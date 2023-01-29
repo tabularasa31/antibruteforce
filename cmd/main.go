30244f7acde0b06e942f5632b6446c7cdbe96673
Tabula Rasa <tabularasa31@gmail.com>
2023-01-29 18:31:21 +0300

    add golangci

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "github.com/tabularasa31/antibruteforce/internal/app"
 
+func main() {
+	app.Run()
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,7 @@
 package app
+
+import "fmt"
+
+func Run() {
+	fmt.Println("Hello, World!")
+}

e53aec9780354bc6807e5c60695f12d77c1bcb1f
Tabula Rasa <tabularasa31@gmail.com>
2023-02-25 23:50:39 +0300

    fix linters

--- config/keys.go
+++ config/keys.go
@@ -3,5 +3,5 @@ package config
 const (
 	envConfigRedisHost     = "APP_REDIS_HOST"
 	envConfigRedisPort     = "APP_REDIS_PORT"
-	envConfigRedisPassword = "APP_REDIS_PASSWORD"
+	envConfigRedisPassword = "APP_REDIS_PASSWORD" //nolint:gosec
 )
